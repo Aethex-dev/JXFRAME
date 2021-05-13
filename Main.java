246afea384a0fe98c2be3e71dad8d19523478349
XfaonXE <73316437+XfaonXE@users.noreply.github.com>
2021-05-13 09:58:53 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package xenonmc.jxframe;
+
+class Main
+{
+    /**
+     * Main class
+     *
+     * @param String args[]
+     */
+    public void main(String args[])
+    {
+        System.out.println("Framework Ready");
+    }
+}
